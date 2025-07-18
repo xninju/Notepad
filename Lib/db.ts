cd991e6b48f6ad3a093a80e10e6eadf57cc818c1
xninju <xninju1@gmail.com>
2025-07-18 08:27:02 +0530

    Create db.ts

---
+++ Lib/db.ts
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+export async function query(text: string, params?: any[]) {
+  const res = await pool.query(text, params);
+  return res;
+}
