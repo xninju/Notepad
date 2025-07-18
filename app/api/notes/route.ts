c11bb4c3622aa78a22aedc3fedc5d59922636112
xninju <xninju1@gmail.com>
2025-07-18 09:11:56 +0530

    Create route.ts

---
+++ app/api/notes/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { sql } from '../../../lib/db';
+
+export async function GET(req: NextRequest) {
+  // ...
+}
+export async function POST(req: NextRequest) {
+  // ...
+}
+export async function DELETE(req: NextRequest) {
+  // ...
+}
